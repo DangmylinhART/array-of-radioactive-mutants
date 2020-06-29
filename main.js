30ccb9122a331d46a08335c4c20e77c18f78972c
Linh My Dang <dangmylinhART@gmail.com>
2020-06-29 17:55:39 -0400

    finished + teacher fixed

--- main.js
+++ main.js
@@ -2,10 +2,87 @@
  * YOUR CODE HERE *
  ******************/
 
+ function addMeToEnd(arr){
+  arr.push('Colin')
+}
 
+////*********** */
 
+function addMeToStart(arr){
+  arr.unshift('Colin')
+}
 
+/////************* */
 
+function changeLast(arr,value){
+  // teacher method
+  arr[arr.length - 1 = value]
+
+  // My method
+  //arr.pop(arr[arr.length -1])
+  //arr.push(value)
+}
+
+/////************* */
+// first solution/wrong
+// function  changeAllValuesTo(arr, value){
+//  arr.splice(arr, arr.length, value) 
+// }
+// let resultX = changeAllValuesTo([1, 2, 3, 5, 7, 9],30)
+// resultX
+
+//*************************** */
+
+function  changeAllValuesTo(arr, value){
+  for( let i = 0 ; i < arr.length ; i++ ){
+  arr[i] = value
+  }
+  return arr
+}
+
+let resultX = changeAllValuesTo([1, 2, 3, 5, 7, 9],30)
+resultX;
+  
+
+///************************* */
+function oddOrEven(arr){
+  for ( let i  = 0 ; i < arr.length ; i++)
+  if (arr[i] % 2 === 0){arr[i] = 'even'}
+  else { arr[i] = 'odd'}
+  return arr
+}
+
+let result3 = oddOrEven([1, 2, 3, 5, 7, 8])
+result3;
+
+////** *///////////
+// wrong method
+// function changeNextThreeToValue(start, arr, value){
+//  arr.splice(start, 3, value)
+// }
+// let result = changeNextThreeToValue( 2,[1, 2, 3, 5, 7, 9], 15)
+// result ;
+
+// function changeNextThreeToValue(start, arr, value){
+//   for ( let  i = 0 ;  i  < arr.length- 1 ; i ++){
+//     // console.log(arr.indexOf(start))
+//     console.log(arr[start])
+//     arr[i]  = value
+//   } return arr 
+//   }
+  
+
+///******************** */
+
+function changeNextThreeToValue(start, arr, value){
+  for ( let  i = start ;  i  < start + 3 ; i ++){
+    arr[i]  = value
+  } return arr
+}
+  let result = changeNextThreeToValue( 1,[1, 2, 3, 5, 7, 9], 15)
+  result ;
+    
+    
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
